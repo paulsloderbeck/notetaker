a1707c1d5aa065f54fb33eacf7bd882e8fab73f8
paulsloderbeck <64672063+paulsloderbeck@users.noreply.github.com>
2020-07-27 21:09:12 -0400

    Created basic server.js with routes to serve index and notes page and begin listening on port 3000

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//basic routes to send user to first page or notes page
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "notes.html"));
+});
+
+
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
