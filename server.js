3a45625e4e2c4679396e32418be39ba8d2f81667
paulsloderbeck <64672063+paulsloderbeck@users.noreply.github.com>
2020-07-28 21:04:05 -0400

    changed file organization to serve static public css and js, added path to serve notes via api

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const path = require("path");
-
+const fs = require("fs");
 const app = express();
 const PORT = 3000;
+const dataPath = "./db/db.json";
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 //basic routes to send user to first page or notes page
 app.get("/", function (req, res) {
@@ -16,6 +18,15 @@ app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "notes.html"));
 });
 
+// get path to read db and return saved notes
+app.get("/api/notes", (req, res) => {
+    fs.readFile(dataPath, "utf8", (err, data) => {
+        if (err) {
+            throw err
+        }
+        res.send(JSON.parse(data));
+    });
+});
 
 
 app.listen(PORT, function () {
